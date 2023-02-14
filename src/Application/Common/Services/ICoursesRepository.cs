ee26e15c6c0f2473247f35d3e1aac394f7655c78
Sebastià Sansó <sanso.barcelo94@gmail.com>
2023-02-14 08:27:56 +0100

    Added get person by id view

--- src/Application/Common/Services/ICoursesRepository.cs
+++ src/Application/Common/Services/ICoursesRepository.cs
@@ -5,5 +5,4 @@ namespace Application.Common.Services;
 public interface ICoursesRepository : IRepository<Course>
 {
     public Task<Course> GetCurrentCoursAsync(CancellationToken ct);
-    public Task<IEnumerable<Course>> GetAllAsync(CancellationToken ct);
 }
