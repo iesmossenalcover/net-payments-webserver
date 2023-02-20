1caf19bf2f60aed589408df3527dbdce01044782
Joan Barcelo Moragues <jbm@smossen.educaib>
2023-02-20 13:47:19 +0100

    updatePerson

--- src/Application/Common/Services/IStudentsRepository.cs
+++ src/Application/Common/Services/IStudentsRepository.cs
@@ -7,4 +7,6 @@ public interface IStudentsRepository : IRepository<Student>
     public Task<Student?> GetStudentByAcademicRecordAsync(long academicRecord, CancellationToken ct);
     public Task<IEnumerable<Student>> GetStudentsByAcademicRecordAsync(IEnumerable<long> academicRecords, CancellationToken ct);
 
+    public Task AddStudentsExistingPersonAsync(Student s, Person p, CancellationToken ct);
+
 }
