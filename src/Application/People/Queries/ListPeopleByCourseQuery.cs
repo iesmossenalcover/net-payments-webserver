829703d3dcfee347817e6217dff2d946b28ae4a1
Sebastià Sansó <sanso.barcelo94@gmail.com>
2023-03-06 07:54:24 +0100

    Change response

--- src/Application/People/Queries/ListPeopleByCourseQuery.cs
+++ src/Application/People/Queries/ListPeopleByCourseQuery.cs
@@ -7,12 +7,12 @@ namespace Application.People.Queries;
 # region ViewModels
 public record CourseVm(long Id, string Name);
 public record PersonSummaryVm(long Id, string DocumentId, string FirstName, string LastName, long GroupId, string GroupName, long? AcademicRecordNumber);
-public record ListPeopleByCourseVm(IEnumerable<PersonSummaryVm> People, long SelectedCourseId);
+public record ListPeopleByCourseVm(IEnumerable<PersonSummaryVm> People);
 #endregion
 
-public record ListPeopleByCourseQuery(long? CourseId) : IRequest<ListPeopleByCourseVm>;
+public record ListPeopleByCourseQuery(long? CourseId) : IRequest<IEnumerable<PersonSummaryVm>>;
 
-public class ListPeopleByCourseQuueryHandler : IRequestHandler<ListPeopleByCourseQuery, ListPeopleByCourseVm>
+public class ListPeopleByCourseQuueryHandler : IRequestHandler<ListPeopleByCourseQuery, IEnumerable<PersonSummaryVm>>
 {
     # region IOC
     private readonly ICoursesRepository _courseRepository;
@@ -26,7 +26,7 @@ public class ListPeopleByCourseQuueryHandler : IRequestHandler<ListPeopleByCours
 
     #endregion
 
-    public async Task<ListPeopleByCourseVm> Handle(ListPeopleByCourseQuery request, CancellationToken ct)
+    public async Task<IEnumerable<PersonSummaryVm>> Handle(ListPeopleByCourseQuery request, CancellationToken ct)
     {
         IEnumerable<Course> courses = await _courseRepository.GetAllAsync(ct);
         Course course = request.CourseId.HasValue ? courses.First(x => x.Id == request.CourseId.Value) : courses.First(x => x.Active);
@@ -37,8 +37,7 @@ public class ListPeopleByCourseQuueryHandler : IRequestHandler<ListPeopleByCours
                     .Skip(0);
                     // .Take(10);
 
-        var peopleVm = personGroupCourses.Select(x => ToPersonVm(x));
-        return new ListPeopleByCourseVm(peopleVm, course.Id);
+        return personGroupCourses.Select(x => ToPersonVm(x));
     }
 
     public static PersonSummaryVm ToPersonVm(PersonGroupCourse pgc)
