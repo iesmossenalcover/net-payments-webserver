73eea75c364e5884dad43e03ad6787df84e078be
sebastiasansobarcelo <114388187+sebastiasansobarcelo@users.noreply.github.com>
2023-03-07 20:57:18 +0100

    Update before_install.sh

--- deploy-scripts/before_install.sh
+++ deploy-scripts/before_install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 rm -rf /var/www/paymentsapi
 rm -rf /etc/systemd/system/paymentsapi.service
-sudo service codedeploy-agent stop
-rm -rf /opt/codedeploy-agent/deployment-root/*
-udo service codedeploy-agent start
