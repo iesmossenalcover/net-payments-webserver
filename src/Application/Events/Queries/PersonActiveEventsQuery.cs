932fb250d68e7b0c4546bb4c412e468e29c462d8
Sebastià Sansó Barceló <sanso.barcelo94@gmail.com>
2023-03-07 07:44:59 +0100

    added display price

--- src/Application/Events/Queries/PersonActiveEventsQuery.cs
+++ src/Application/Events/Queries/PersonActiveEventsQuery.cs
@@ -9,7 +9,7 @@ namespace Application.Events.Queries;
 
 # region ViewModels
 public record PersonVm(string DocumentId, string FullName);
-public record PublicEventVm(string Code, string Name, decimal Price, bool selectable);
+public record PublicEventVm(string Code, string Name, decimal Price, string DisplayPrice, bool selectable);
 public record PersonActiveEventsVm(IEnumerable<PublicEventVm> Events, PersonVm person);
 #endregion
 
@@ -47,7 +47,7 @@ public class PersonActiveEventsQueryHandler : IRequestHandler<PersonActiveEvents
         personEvents = personEvents.Where(x => x.Event.IsActive && !x.Paid);
 
         // TODO: Decide with events are selectable, for the moment all are selectable
-        IEnumerable<PublicEventVm> eventsVm = personEvents.Select(x => new PublicEventVm(x.Event.Code, x.Event.Name, pgc.PriceForEvent(x.Event), true));
+        IEnumerable<PublicEventVm> eventsVm = personEvents.Select(x => new PublicEventVm(x.Event.Code, x.Event.Name, pgc.PriceForEvent(x.Event), $"{pgc.PriceForEvent(x.Event)} €", true));
 
         return Response<PersonActiveEventsVm>.Ok(
             new PersonActiveEventsVm(eventsVm, new PersonVm(person.DocumentId, $"{person.Name} {person.Surname1} {person.Surname2}"))
