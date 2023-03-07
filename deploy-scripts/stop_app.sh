bc1f3ea0db4763fadc9055e333162206614acaca
sebastiasansobarcelo <114388187+sebastiasansobarcelo@users.noreply.github.com>
2023-03-07 20:57:30 +0100

    Update stop_app.sh

--- deploy-scripts/stop_app.sh
+++ deploy-scripts/stop_app.sh
@@ -1,2 +1,5 @@
 systemctl stop paymentsapi.service
 systemctl disable paymentsapi.service
+sudo service codedeploy-agent stop
+rm -rf /opt/codedeploy-agent/deployment-root/*
+udo service codedeploy-agent start
