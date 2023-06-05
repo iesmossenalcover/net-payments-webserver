84e159584987aada099c7d353841f71c70172291
Sebastià Sansó Barceló <sanso.barcelo94@gmail.com>
2023-06-05 08:45:00 +0200

    fix migrations

---
+++ Migrations/20230605064401_ChangeLastName.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace netpaymentswebserver.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeLastName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_person_Surname2",
+                schema: "main",
+                table: "person");
+
+            migrationBuilder.DropColumn(
+                name: "Surname2",
+                schema: "main",
+                table: "person");
+
+            migrationBuilder.RenameColumn(
+                name: "Surname1",
+                schema: "main",
+                table: "person",
+                newName: "LastName");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_person_Surname1",
+                schema: "main",
+                table: "person",
+                newName: "IX_person_LastName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                schema: "main",
+                table: "person",
+                newName: "Surname1");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_person_LastName",
+                schema: "main",
+                table: "person",
+                newName: "IX_person_Surname1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname2",
+                schema: "main",
+                table: "person",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_person_Surname2",
+                schema: "main",
+                table: "person",
+                column: "Surname2");
+        }
+    }
+}
