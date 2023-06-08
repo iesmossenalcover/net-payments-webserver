03cf62c35c23eeeaf596de956a3ffeed34a152f5
Sebastià Sansó Barceló <sanso.barcelo94@gmail.com>
2023-06-08 08:43:16 +0200

    fix lastname

--- src/Application/People/Queries/GetPeopleQuery.cs
+++ src/Application/People/Queries/GetPeopleQuery.cs
@@ -24,6 +24,6 @@ public class GetPeopleQueryHandler : IRequestHandler<GetPeopleQuery, IEnumerable
     public async Task<IEnumerable<PersonRowVm>> Handle(GetPeopleQuery request, CancellationToken ct)
     {
         IEnumerable<PersonGroupCourse> ppgc = await _personGroupCourseRepository.FilterPeople(new FilterPeople(request.Query), 50, ct);
-        return ppgc.Select(x => new PersonRowVm(x.PersonId, x.Person.DocumentId, x.Person.Name, x.Person.Surname1, x.Person.Surname2, x.GroupId, x.Group.Name, x.Amipa, x.Person.AcademicRecordNumber));
+        return ppgc.Select(x => new PersonRowVm(x.PersonId, x.Person.DocumentId, x.Person.Name, x.Person.LastName, x.GroupId, x.Group.Name, x.Amipa, x.Person.AcademicRecordNumber));
     }
 }
