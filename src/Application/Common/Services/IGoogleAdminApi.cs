e941f673c291b990294a645857732df7d6c1f46c
Joan Barcelo Moragues <jbm@smossen.educaib>
2023-06-12 11:06:54 +0200

    getallusers admingoogle

--- src/Application/Common/Services/IGoogleAdminApi.cs
+++ src/Application/Common/Services/IGoogleAdminApi.cs
@@ -1,7 +1,20 @@
+using Application.Common.Models;
+
 namespace Application.Common.Services;
 
 public interface IGoogleAdminApi
 {
-    public Task<IEnumerable<string>> GetUserClaims(string email, CancellationToken ct);
     public Task Test(CancellationToken ct);
+    public Task<IEnumerable<string>> GetUserClaims(string email, CancellationToken ct);
+    public Task<GoogleApiResult<bool>> SuspendByOU(string ouPath);
+    public Task<GoogleApiResult<bool>> MoveUserToOU(string email, string ouPath);
+    public Task<GoogleApiResult<bool>> CreateUser(
+        string email,
+        string firstName,
+        string lastName,
+        string password,
+        string ouPath,
+        bool changePasswordNexLogin = false
+    );
+
 }
--- src/Infrastructure/GoogleAdminApi.cs
+++ src/Infrastructure/GoogleAdminApi.cs
@@ -1,3 +1,4 @@
+using Application.Common.Models;
 using Application.Common.Services;
 using Domain.Entities.Authentication;
 using Google.Apis.Admin.Directory.directory_v1;
@@ -92,6 +93,18 @@ public class GoogleAdminApi : IGoogleAdminApi
 
     public async Task Test(CancellationToken ct)
     {
+        DirectoryService service = CreateService();
+
+        // Move to UO
+        // GoogleApiResult<bool> response = await MoveUserToOU(service, "noreply@iesmossenalcover.cat", "/exalumnes");
+        // var userResult = await CreateUser(service, "test1234@iesmossenalcover.cat", "Prova", "Prova1 Prova2", "12345678", "/alumnes/bat/1rbat/grup L");
+        // Console.WriteLine(userResult.Success);
+        var suspendResult = await GetAllUsers("/alumnes");
+        Console.WriteLine(suspendResult.Success);
+    }
+
+    public DirectoryService CreateService()
+    {
         GoogleCredential credential = GoogleCredential.FromFile(CredentialFilePath);
         // Specify the scope of access and enable domain-wide delegation.
         string[] scopes = new string[]
@@ -109,22 +122,16 @@ public class GoogleAdminApi : IGoogleAdminApi
             HttpClientInitializer = credential,
             ApplicationName = ApplicationName,
         });
-
-        // Move to UO
-        // ApiResult<bool> response = await MoveUserToOU(service, "noreply@iesmossenalcover.cat", "/exalumnes");
-        // var userResult = await CreateUser(service, "test1234@iesmossenalcover.cat", "Prova", "Prova1 Prova2", "12345678", "/alumnes/bat/1rbat/grup L");
-        // Console.WriteLine(userResult.Success);
-        var suspendResult = await SuspendByOU(service, "/alumnes/BAT/1rbat/grup L");
-        Console.WriteLine(suspendResult.Success);
+        return service;
     }
 
-    private async Task<ApiResult<bool>> SuspendByOU(
-        DirectoryService service,
+    public async Task<GoogleApiResult<bool>> SuspendByOU(
         string ouPath
         )
     {
         try
         {
+            DirectoryService service = CreateService();
             UsersResource.ListRequest userListRequest = service.Users.List();
             userListRequest.Query = $"orgUnitPath='{ouPath}'";
             userListRequest.Domain = Domain;
@@ -146,16 +153,15 @@ public class GoogleAdminApi : IGoogleAdminApi
             }
             while (!string.IsNullOrEmpty(users.NextPageToken));
 
-            return new ApiResult<bool>(true);
+            return new GoogleApiResult<bool>(true);
         }
         catch (System.Exception e)
         {
-            return new ApiResult<bool>(e.Message);
+            return new GoogleApiResult<bool>(e.Message);
         }
     }
 
-    private async Task<ApiResult<Google.Apis.Admin.Directory.directory_v1.Data.User>> CreateUser(
-        DirectoryService service,
+    public async Task<GoogleApiResult<bool>> CreateUser(
         string email,
         string firstName,
         string lastName,
@@ -174,55 +180,72 @@ public class GoogleAdminApi : IGoogleAdminApi
         };
         try
         {
+            DirectoryService service = CreateService();
             newUser = await service.Users.Insert(newUser).ExecuteAsync();
-            return new ApiResult<Google.Apis.Admin.Directory.directory_v1.Data.User>(newUser);
+            return new GoogleApiResult<bool>(true);
         }
         catch (System.Exception e)
         {
-            return new ApiResult<Google.Apis.Admin.Directory.directory_v1.Data.User>(e.Message);
+            return new GoogleApiResult<bool>(e.Message);
         }
     }
 
-    private async Task<ApiResult<bool>> MoveUserToOU(DirectoryService service, string email, string ouPath)
+    public async Task<GoogleApiResult<bool>> MoveUserToOU(string email, string ouPath)
     {
         try
         {
+            DirectoryService service = CreateService();
             string memberId = email;
             var memberRequest = service.Users.Get(memberId);
             var member = await memberRequest.ExecuteAsync();
             if (member == null)
             {
-                return new ApiResult<bool>("user not found");
+                return new GoogleApiResult<bool>("user not found");
             }
 
             member.OrgUnitPath = ouPath;
 
             var updateRequest = service.Users.Update(member, memberId);
             member = await updateRequest.ExecuteAsync();
-            return new ApiResult<bool>(true);
+            return new GoogleApiResult<bool>(true);
         }
         catch (System.Exception e)
         {
-            return new ApiResult<bool>(e.Message);
+            return new GoogleApiResult<bool>(e.Message);
         }
     }
 
-    private class ApiResult<T>
+    public async Task<GoogleApiResult<IEnumerable<string>>> GetAllUsers(string ouPath)
     {
-        public bool Success { get; set; }
-        public string? ErrorMessage { get; set; } = null;
-        public T? Data { get; set; }
-
-        public ApiResult(string message)
+        try
         {
-            Success = false;
-            ErrorMessage = message;
+            DirectoryService service = CreateService();
+            List<string> usersList = new List<string>();
+            UsersResource.ListRequest userListRequest = service.Users.List();
+            userListRequest.Query = $"orgUnitPath='{ouPath}'";
+            userListRequest.Domain = Domain;
+            userListRequest.MaxResults = 50;
+            Users users;
+
+            do
+            {
+                users = await userListRequest.ExecuteAsync();
+
+                foreach (var user in users.UsersValue)
+                {
+                    usersList.Add(user.PrimaryEmail);
+                }
+                userListRequest.PageToken = users.NextPageToken;
+            }
+            while (!string.IsNullOrEmpty(users.NextPageToken));
+
+            return new GoogleApiResult<IEnumerable<string>>(usersList.AsEnumerable());
         }
-
-        public ApiResult(T data)
+        catch (System.Exception e)
         {
-            Success = true;
-            Data = data;
+            return new GoogleApiResult<IEnumerable<string>>(e.Message);
         }
     }
+
+
 }
