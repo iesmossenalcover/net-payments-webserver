df6e0b3cca5a59a802d8526e08a6cccd24b90c52
Sebastià Sansó Barceló <sanso.barcelo94@gmail.com>
2023-06-12 09:37:32 +0200

    remove email, phone and amipa from batch uplooad

--- src/Application/Common/Models/BatchUploadRowModel.cs
+++ src/Application/Common/Models/BatchUploadRowModel.cs
@@ -7,10 +7,8 @@
         public string Nom { get; set; } = default!;
         public string Llinatge1 { get; set; } = default!;
         public string? Llinatge2 { get; set; }
-        public string? EmailContacte { get; set; }
         public string? TelContacte { get; set; }
         public string? Grup { get; set; }
-        public string? Amipa { get; set; }
         public string? Assignatures { get; set; }
     }
 }
--- src/Infrastructure/CsvParsers.cs
+++ src/Infrastructure/CsvParsers.cs
@@ -36,10 +36,8 @@ public class BatchUploadRowModelMap : ClassMap<BatchUploadRowModel>
         Map(m => m.Identitat).Validate(x => !string.IsNullOrEmpty(x.Field));
         Map(m => m.Nom).Validate(x => !string.IsNullOrEmpty(x.Field));
         Map(m => m.Llinatge1).Validate(x => !string.IsNullOrEmpty(x.Field));
-        Map(m => m.EmailContacte);
         Map(m => m.TelContacte);
         Map(m => m.Grup).Validate(x => !string.IsNullOrEmpty(x.Field));
-        Map(m => m.Amipa);
         Map(m => m.Assignatures);
     }
 }
