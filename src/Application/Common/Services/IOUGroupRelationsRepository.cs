e377c62ecf9e091b3bcae6f4289ec2feadfd91df
Sebastià Sansó Barceló <sanso.barcelo94@gmail.com>
2023-06-13 11:34:07 +0200

    Added migrations

---
+++ src/Application/Common/Services/IOUGroupRelationsRepository.cs
@@ -0,0 +1,5 @@
+namespace Application.Common.Services;
+
+public interface IOUGroupRelationsRepository
+{
+}
--- src/Infrastructure/Repos/UoGroupRelationRepository.cs
+++ src/Infrastructure/Repos/UoGroupRelationRepository.cs
@@ -1,8 +1,9 @@
+using Application.Common.Services;
 using Domain.Entities.GoogleApi;
 
 namespace Infrastructure.Repos;
 
-public class UoGroupRelationRepository : Repository<UoGroupRelation>
+public class UoGroupRelationRepository : Repository<UoGroupRelation>, IOUGroupRelationsRepository
 {
     public UoGroupRelationRepository(AppDbContext dbContext) : base(dbContext, dbContext.UoGroupRelations) { }
 }
