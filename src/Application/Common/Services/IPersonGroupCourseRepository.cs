3ad76e77ce456b53843b03a2da99451065d74ce0
Sebastià Sansó Barceló <sanso.barcelo94@gmail.com>
2023-06-16 09:39:52 +0200

    Add support to view old events

--- src/Application/Common/Services/IPersonGroupCourseRepository.cs
+++ src/Application/Common/Services/IPersonGroupCourseRepository.cs
@@ -8,6 +8,7 @@ public interface IPersonGroupCourseRepository : IRepository<PersonGroupCourse>
     public Task<IEnumerable<PersonGroupCourse>> GetPersonGroupCoursesByPersonIdAsync(long personId, CancellationToken ct);
 
     public Task<IEnumerable<PersonGroupCourse>> GetCurrentCourseGroupByPeopleIdsAsync(IEnumerable<long> peopleIds, CancellationToken ct);
+    public Task<IEnumerable<PersonGroupCourse>> GetPeopleGroupByPeopleIdsAndCourseIdAsync(long courseId, IEnumerable<long> peopleIds, CancellationToken ct);
     public Task<PersonGroupCourse?> GetCoursePersonGroupByDocumentId(string documentId, long courseId, CancellationToken ct);
     public Task<PersonGroupCourse?> GetCoursePersonGroupById(long personId, long courseId, CancellationToken ct);
     public Task<IEnumerable<PersonGroupCourse>> FilterPeople(FilterPeople filter, int maxResults, CancellationToken ct);
