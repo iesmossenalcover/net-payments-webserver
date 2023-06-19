e9d61d5965496d0b9cff3de52e133e4e1fe0e5d5
Sebastià Sansó Barceló <sanso.barcelo94@gmail.com>
2023-06-19 09:36:32 +0200

    fix generate email

--- src/Application/Common/Helpers/Email.cs
+++ src/Application/Common/Helpers/Email.cs
@@ -10,17 +10,6 @@ public class Email
 
     public static string NormalizeText(string text)
     {
-        // string final = text.Replace('à','a');
-        // final = text.Replace('á','a');
-        // final = text.Replace('è','e');
-        // final = text.Replace('é','e');
-        // final = text.Replace('ì','i');
-        // final = text.Replace('í','i');
-        // final = text.Replace('ó','o');
-        // final = text.Replace('ò','o');
-        // final = text.Replace('ù','u');
-        // final = text.Replace('ú','u');
-
         text = text.Replace("ñ", "ny");
         text = text.Replace("l·l", "ll");
         text = text.Replace(".", "");
