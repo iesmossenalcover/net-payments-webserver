35977d825916f3e58ae2026ba266c0747a7dafc4
Sebastià Sansó <sanso.barcelo94@gmail.com>
2023-07-03 09:10:11 +0200

    Added create new course

--- src/WebServer/Handlers/Courses.cs
+++ src/WebServer/Handlers/Courses.cs
@@ -1,12 +1,44 @@
+using Application.Common;
 using Application.Common.Models;
+using Application.Courses.Commands;
 using Application.Courses.Queries;
-using Application.People.Queries;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebServer.Handlers;
 
 public class Courses
 {
+    public static async Task<Response<long>> CreateCourse(
+        [FromBody] CreateCourseCommand cmd,
+        IMediator mediator)
+    {
+        return await mediator.Send(cmd);
+    }
+
+    public static async Task<Response<Unit>> UpdateCourse(
+        long id,
+        [FromBody] UpdateCourseCommand cmd,
+        IMediator mediator)
+    {
+        cmd.SetId(id);
+        return await mediator.Send(cmd);
+    }
+
+    public static async Task<Response<Unit>> SetActiveCourse(
+        long id,
+        IMediator mediator)
+    {
+        var cmd = new SetActiveCourseCommand();
+        cmd.SetId(id);
+        return await mediator.Send(cmd);
+    }
+
+    public static async Task<IEnumerable<CourseVm>> GetAllCourses(IMediator mediator)
+    {
+        return await mediator.Send(new GetCoursesQuery());
+    }
+
     public static async Task<SelectorVm> GetCoursesSelector(IMediator mediator)
     {
         return await mediator.Send(new GetAllCoursesSelectorQuery());
