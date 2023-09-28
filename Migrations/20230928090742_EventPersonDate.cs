ac21bf4f66644590b13c09f573f1d2224b38fe02
Sebastià Sansó Barceló <sanso.barcelo94@gmail.com>
2023-09-28 11:08:16 +0200

    Added migration

---
+++ Migrations/20230928090742_EventPersonDate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace netpaymentswebserver.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventPersonDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DatePaid",
+                schema: "main",
+                table: "event_person",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DatePaid",
+                schema: "main",
+                table: "event_person");
+        }
+    }
+}
